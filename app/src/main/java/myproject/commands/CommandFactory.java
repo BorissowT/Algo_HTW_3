0f05bb7b70be48d6c9b361ec740a2af99cddd1c7
timofeyborisov <tbs999borisov@gmail.com>
2021-11-29 17:45:44 +0100

    add push impl

--- app/src/main/java/myproject/commands/CommandFactory.java
+++ app/src/main/java/myproject/commands/CommandFactory.java
@@ -2,12 +2,16 @@ package myproject.commands;
 
 import myproject.commands.Impl.*;
 import myproject.console.IConsole;
+import myproject.data.Student;
+import myproject.stack.IStackable;
+import myproject.stack.Stack;
 
 import java.util.LinkedList;
 
 public class CommandFactory {
 
 	final private IConsole console;
+	IStackable<Student> stack = new Stack();
 
 	public CommandFactory(IConsole console) {
 		super();
@@ -17,7 +21,7 @@ public class CommandFactory {
 		LinkedList<ICommand> cmds = new LinkedList<ICommand>();
 
 		cmds.add(new ExitProgramCmd());
-		cmds.add(new PushCmd());
+		cmds.add(new PushCmd(console, stack));
 		cmds.add(new PopCmd());
 		cmds.add(new PeekCmd());
 		cmds.add(new IsEmptyCmd());
--- app/src/main/java/myproject/commands/Impl/PushCmd.java
+++ app/src/main/java/myproject/commands/Impl/PushCmd.java
@@ -1,12 +1,29 @@
 package myproject.commands.Impl;
 
 import myproject.commands.ICommand;
+import myproject.console.IConsole;
+import myproject.data.Student;
+import myproject.stack.IStackable;
+
 
 public class PushCmd implements ICommand {
 
+	final private IConsole console;
+	private final IStackable<Student> stack;
+
+	public PushCmd(IConsole console, IStackable<Student> stack) {
+		this.console = console;
+		this.stack = stack;
+	}
+
 	@Override
 	public void execute() {
-
+		String prename = console.readString("Please type firstname of the student");
+		String surname = console.readString("Please lastname name of the student");
+		int matriculationNumber = console.readInteger("Please type matriculation number of the student");
+		int course = console.readInteger("Please type course number of the student");
+		Student student = new Student(prename, surname, matriculationNumber, course);
+		stack.push(student);
 	}
 	@Override
 	public String toString() {
