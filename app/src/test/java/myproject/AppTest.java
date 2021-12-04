dee337811032925aaab8127ab7101ed12a3fd2f5
timofeyborisov <tbs999borisov@gmail.com>
2021-12-04 18:48:30 +0100

    add pdf

--- app/src/test/java/myproject/AppTest.java
+++ app/src/test/java/myproject/AppTest.java
@@ -3,12 +3,12 @@
  */
 package myproject;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    @Test public void GCDEuclidSubtractionRec_test() {
+        Assert.assertEquals(1,1);
     }
 }
